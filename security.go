8795614d8e45887898c2bdbbe06158579c6a8635
fundon <cfddream@gmail.com>
2014-07-15 18:54:15 +0800

    security set

--- security.go
+++ security.go
@@ -16,11 +16,28 @@ var cmdSecurity = cli.Command{
 			Usage:  "Get system security's status",
 			Action: runSecurityGet,
 		},
+		{
+			Name:   "set",
+			Usage:  "set system security's status",
+			Action: runSecuritySet,
+		},
 	},
 }
 
 func runSecurityGet(c *cli.Context) {
 	req, err := client.NewRequest("GET", "/security/get_config")
+	if err != nil {
+		fmt.Println(err)
+	}
+	req.SetCookie(config.Cookie)
+	var result api.SecurityEnabled
+	err = req.ToJSON(&result)
+	fmt.Println(result.Code)
+	fmt.Println(result.Enabled)
+}
+
+func runSecuritySet(c *cli.Context) {
+	req, err := client.NewRequest("POST", "/security/set_config")
 	if err != nil {
 		fmt.Println(err)
 	}
